5ece74790cf46f45f74995e3f2581f91328d5963
Himura Kenji <aaron.ariperto@gmail.com>
2019-11-06 19:41:51 -0300

    Proyecto creado - Contiene Core - Mockito - Data

---
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/PizzeriaBoundariesApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.pizzeriaboundaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaBoundariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaBoundariesApplication.class, args);
+    }
+
+}
---
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/PizzeriaBoundariesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.pizzeriaboundaries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaBoundariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
