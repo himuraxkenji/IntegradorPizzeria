d52c22b7ed4939f2fadf5319554ecf42ab106b02
juan asis <juan.asis1234gmail.com>
2019-11-07 00:10:10 -0300

    PizzaUnitTest
    modificacion test ClienteUnitTest
    -pizza instancia correcta
    -pizza sin nombre instancia no creada

--- Pizzeria-Core/src/test/java/modeloUnitTest/ClienteUnitTest.java
+++ Pizzeria-Core/src/test/java/modeloUnitTest/ClienteUnitTest.java
@@ -1,17 +1,25 @@
 package modeloUnitTest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class ClienteUnitTest {
     @Test
     void instanciarClienteClienteCompletoInstanciaCorrecta() {
-        Cliente elCliente=Cliente.factoryCliente(1,"Juan Asis","Timoteo Gordillo 43","Centro","39400500");
+        Barrio elBarrio=Barrio.factoryBarrio(1,"Centro");
+        Cliente elCliente=Cliente.factoryCliente(1,"Juan Asis","Timoteo Gordillo 43",elBarrio.getNombre(),"39400500");
         assertNotNull(elCliente);
     }
 
     @Test
     void instanciarClienteClienteSinDocumentoClienteInstanciaIncorrecta() {
         Assertions.assertThrows(ClienteSinDocumentoException.class, ()-> Cliente.factoryCliente(1,"Juan Asis","Timoteo Gordillo 43","Centro","39400500") );
+    }
+
+
     }
     
 }
