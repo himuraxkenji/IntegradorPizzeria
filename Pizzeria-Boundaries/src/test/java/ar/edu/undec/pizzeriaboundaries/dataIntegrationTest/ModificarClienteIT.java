74b7068d5b36b1f097f29c61eb34ee7c33e09ea1
Riperto Adriel Aaron <aaron.ariperto@gmai.com>
2019-11-12 19:04:34 -0300

    Modificar Cliente: Test de integracion

--- Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ModificarClienteIT.java
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ModificarClienteIT.java
@@ -14,8 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -25,29 +23,17 @@ public class ModificarClienteIT {
     @Autowired
     ActualizarClienteRepoImpl actualizarClienteRepo;
 
-    @Test
-    @SqlGroup({
-            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:actualizarClienteAntesUno.sql"),
+   @SqlGroup({
+            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:actualizarClienteAntes.sql"),
             @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:actualizarClienteDespues.sql")
     })
+    @Test
     public void actualizarCliente_ClienteGuardado_devuelveTrue() throws BarrioIncompletoException, ClienteIncompletoException {
-        Barrio barrio = Barrio.factoryBarrio(1, "80 Lotes");
-        Cliente cliente= Cliente.factoryCliente(1, "Santiago","San Roman", barrio,"43.226.263");
+        Barrio barrio = Barrio.factoryBarrio(1, "Altos de Chilecito");
+        Cliente cliente= Cliente.factoryCliente(1, "Santiago","San Roman", barrio,"12312312");
         boolean resultado= actualizarClienteRepo.actualizar(cliente);
         assertTrue(resultado);
     }
 
-    @Test
-    @SqlGroup({
-            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:actualizarClienteAntesDos.sql"),
-            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:actualizarClienteDespues.sql")
-    })
-    public void actualizarCliente_ClienteConOtroId_devuelveFalse() throws BarrioIncompletoException, ClienteIncompletoException {
-        Barrio barrio = Barrio.factoryBarrio(1, "80 Lotes");
-        Cliente clienteDatosNuevos= Cliente.factoryCliente(1, "Renegado","San Roman", barrio,"43.226.263");
-        actualizarClienteRepo.actualizar(clienteDatosNuevos);
-        boolean resultado= actualizarClienteRepo.actualizar(clienteDatosNuevos);
-        assertFalse(resultado);
-    }
 
 }
