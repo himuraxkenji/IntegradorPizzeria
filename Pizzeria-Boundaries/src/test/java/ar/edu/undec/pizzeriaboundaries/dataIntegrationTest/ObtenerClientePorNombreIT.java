2eb6e930bbaa9606d1a6922aa289db4828b72714
soriajose <soriaajose@gmail.com>
2019-11-12 18:48:49 -0300

    Se añadio archivo sql "obtenerClientePorNombreDespues"

--- Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ObtenerClientePorNombreIT.java
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ObtenerClientePorNombreIT.java
@@ -4,11 +4,18 @@ import excepciones.BarrioIncompletoException;
 import excepciones.ClienteIncompletoException;
 import modelo.Barrio;
 import modelo.Cliente;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
 @SpringBootTest
 public class ObtenerClientePorNombreIT {
 
+    @Test
+    @SqlGroup({
+            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:obtenerClientePorNombreAntes.sql"),
+            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:obtenerClientePorNombreDespues.sql")})
 
     public void obtenerClientePorNombre_clienteDevuelto_clienteCorrecto() throws BarrioIncompletoException, ClienteIncompletoException {
 
@@ -21,6 +28,9 @@ public class ObtenerClientePorNombreIT {
         Cliente elCliente = Cliente.factoryCliente(1,"Jose","Castro y Bazan", elBarrio,"39300672");
         Cliente elCliente2 = Cliente.factoryCliente(1,"Juan","9 de julio", elBarrio2,"39300672");
         Cliente elCliente3 = Cliente.factoryCliente(1,"Pepito","CualquierCalle", elBarrio3,"39300672");
+
+
+
 
     }
 
