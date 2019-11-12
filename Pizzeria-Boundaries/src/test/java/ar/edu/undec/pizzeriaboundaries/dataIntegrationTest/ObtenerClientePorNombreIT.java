b2ae0c817ae1ff7401ca6a76354265fcb07fc102
soriajose <soriaajose@gmail.com>
2019-11-12 18:37:11 -0300

    "Se creo test de integracion obtenerClientePorNombreIT"

---
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ObtenerClientePorNombreIT.java
@@ -0,0 +1,17 @@
+package ar.edu.undec.pizzeriaboundaries.dataIntegrationTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ObtenerClientePorNombreIT {
+
+
+    public void obtenerClientePorNombre_clienteDevuelto_clienteCorrecto(){
+
+        
+
+    }
+
+
+
+}
