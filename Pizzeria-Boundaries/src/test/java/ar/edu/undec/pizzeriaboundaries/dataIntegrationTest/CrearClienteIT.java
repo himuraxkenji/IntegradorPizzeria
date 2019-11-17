428715c2dd86afce3b6a9c5bd7af232ac0c2ac97
soriajose <soriaajose@gmail.com>
2019-11-16 22:23:13 -0300

    "Se creo test de Integracion crearClienteIT"
    Primero se creo el test, luego se crean los repositorios

---
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/CrearClienteIT.java
@@ -0,0 +1,35 @@
+package ar.edu.undec.pizzeriaboundaries.dataIntegrationTest;
+
+
+import excepciones.BarrioIncompletoException;
+import excepciones.ClienteIncompletoException;
+import modelo.Barrio;
+import modelo.Cliente;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrearClienteIT {
+
+    @Autowired
+    CrearClienteRepoImpl crearClienteRepoImpl;
+
+    @Test
+    public void crearCliente_clienteNoExistente_creadoCorrectamente() throws BarrioIncompletoException, ClienteIncompletoException {
+
+        Barrio miBarrio = Barrio.factoryBarrio(1, "Castro y Bazan");
+        Cliente cliente = Cliente.factoryCliente(1, "Jose Soria", "Castro y Bazan", miBarrio, "39300672");
+        boolean respuesta = crearClienteRepoImpl.save(cliente);
+
+        assertTrue(respuesta);
+
+    }
+
+
+}
