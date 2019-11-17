c0a6343758061700ac5937cb5ca1da2abdfb9ca0
soriajose <soriaajose@gmail.com>
2019-11-16 22:36:53 -0300

    "Se creo ICrearClienteCRUD en repositorioCRUD"

---
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Data/RepositorioCRUD/ICrearClienteCRUD.java
@@ -0,0 +1,12 @@
+package ar.edu.undec.pizzeriaboundaries.Data.RepositorioCRUD;
+
+import ar.edu.undec.pizzeriaboundaries.Data.ModelosEntity.ClienteEntity;
+import modelo.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICrearClienteCRUD extends CrudRepository<ClienteEntity, Integer> {
+
+    boolean save(Cliente cliente);
+
+
+}
