0f659cb4bf3a4a58b8774d228ed6db614adef3c1
Riperto Adriel Aaron <aaron.ariperto@gmai.com>
2019-11-18 01:29:29 -0300

    Modificar Cliente: DTO Barrio y Cliente

--- Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Service/ModeloService/BarrioDTO.java
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Service/ModeloService/BarrioDTO.java
@@ -1,6 +1,34 @@
 package ar.edu.undec.pizzeriaboundaries.Service.ModeloService;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BarrioDTO {
-    public BarrioDTO(int i, String altos_de_chilecito) {
+
+    @JsonProperty("idBarrio")
+    private int idBarrio;
+
+    @JsonProperty("nombre")
+    private String nombre;
+
+    public BarrioDTO(@JsonProperty("id") Integer id,
+                     @JsonProperty("nombre") String nombre) {
+        this.idBarrio = id;
+        this.nombre = nombre;
+    }
+
+    public int getIdBarrio() {
+        return idBarrio;
+    }
+
+    public void setIdBarrio(int idBarrio) {
+        this.idBarrio = idBarrio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
