5c48a239bcc73edbbfb3c506f5f8b054eb52c4c8
soriajose <soriaajose@gmail.com>
2019-11-18 16:12:22 -0300

    "Se agrego metodo  obtenerClientePorNombreUseCase en UseCaseConfig"

--- Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Service/Config/UseCaseConfig.java
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Service/Config/UseCaseConfig.java
@@ -1,10 +1,12 @@
 package ar.edu.undec.pizzeriaboundaries.Service.Config;
 
 import interactor.ModificarClienteUseCase;
+import interactor.ObtenerClientePorNombreUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import repositorio.IRepositorioModificarCliente;
+import repositorio.IRepositorioObtenerClientePorNombre;
 
 @Configuration
 public class UseCaseConfig {
@@ -13,8 +15,17 @@ public class UseCaseConfig {
     @Autowired
     private IRepositorioModificarCliente iRepositorioModificarCliente;
 
+    @Autowired
+    private IRepositorioObtenerClientePorNombre iRepositorioObtenerClientePorNombre;
+
     @Bean
     public ModificarClienteUseCase modificarClienteUseCase(){
         return new ModificarClienteUseCase(iRepositorioModificarCliente);
     }
+
+    @Bean
+    public ObtenerClientePorNombreUseCase obtenerClientePorNombreUseCase(){
+        return new ObtenerClientePorNombreUseCase(iRepositorioObtenerClientePorNombre);
+    }
+
 }
