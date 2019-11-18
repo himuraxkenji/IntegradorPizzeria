0918be0fbc7077a0746d34ce5173483f109f4454
Riperto Adriel Aaron <aaron.ariperto@gmai.com>
2019-11-18 18:14:57 -0300

    Crear Barrio: Test de integracion Data

---
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/CrearBarrioIT.java
@@ -0,0 +1,36 @@
+package ar.edu.undec.pizzeriaboundaries.dataIntegrationTest;
+
+
+import ar.edu.undec.pizzeriaboundaries.Data.RepositorioImplementacion.CrearBarrioRepoImpl;
+import excepciones.BarrioIncompletoException;
+import modelo.Barrio;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrearBarrioIT {
+
+    @Autowired
+    CrearBarrioRepoImpl crearBarrioRepo;
+
+    @Test
+    @SqlGroup({
+            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:crearBarrioAntesUno.sql"),
+            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:crearBarrioDespues.sql")
+    })
+    public void crearBarrio_BarrioNoExiste_BarrioGuardado() throws BarrioIncompletoException {
+        Barrio barrio = Barrio.factoryBarrio(1, "Castro y Bazan");
+        boolean resultado = crearBarrioRepo.guardar(barrio);
+        Assert.assertTrue(resultado);
+    }
+
+
+
+}
