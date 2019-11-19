95681301882ee3954f0672a9329a70ded4c63bcc
soriajose <soriaajose@gmail.com>
2019-11-19 08:59:31 -0300

    "Se agrega modificarPizzaIT a dataIntegracionTest"

---
+++ Pizzeria-Boundaries/src/test/java/ar/edu/undec/pizzeriaboundaries/dataIntegrationTest/ModificarPizzaIT.java
@@ -0,0 +1,38 @@
+package ar.edu.undec.pizzeriaboundaries.dataIntegrationTest;
+
+import excepciones.PizzaIncompletaException;
+import modelo.Pizza;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModificarPizzaIT {
+
+    @Autowired
+    ModificarPizzaRepoImpl modificarPizzaRepo;
+
+    @SqlGroup({
+            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:actualizarPizzaAntes.sql"),
+            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:actualizarPizzaDespues.sql")
+    })
+
+    @Test
+    public void actualizarPizza_nuevaPizza_devuelveTrue() throws PizzaIncompletaException {
+
+        Pizza pizza = Pizza.factoryPizza(1, "Napolitana", 250.00f, 15);
+
+        boolean respuesta = modificarPizzaRepo.actualizar(pizza);
+
+        assertTrue(respuesta);
+        
+    }
+
+}
