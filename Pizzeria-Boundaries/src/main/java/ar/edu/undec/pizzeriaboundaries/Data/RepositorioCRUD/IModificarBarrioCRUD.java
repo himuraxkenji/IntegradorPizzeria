772ac44b8cf3d6d00be08f430cf3bb14a92617d0
juan asis <juan.asis1234gmail.com>
2019-11-19 18:21:11 -0300

    Revert "correcion de errores Test ModificarBarrioIT"
    This reverts commit a4de18d7b15e382dfdd6c586cdb2d35344bf3b69.

--- Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Data/RepositorioCRUD/IModificarBarrioCRUD.java
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Data/RepositorioCRUD/IModificarBarrioCRUD.java
@@ -3,9 +3,7 @@ package ar.edu.undec.pizzeriaboundaries.Data.RepositorioCRUD;
 import ar.edu.undec.pizzeriaboundaries.Data.ModelosEntity.BarrioEntity;
 import modelo.Barrio;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IModificarBarrioCRUD extends CrudRepository<BarrioEntity, Integer> {
     BarrioEntity findByNombre(String nombre);
 }
