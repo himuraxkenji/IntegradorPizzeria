33336ca9423472d1493bf04ba0af97df9f990211
soriajose <soriaajose@gmail.com>
2019-11-19 10:35:20 -0300

    "Se agrego PizzaDTOMapper a serviceMapper"

---
+++ Pizzeria-Boundaries/src/main/java/ar/edu/undec/pizzeriaboundaries/Service/ServiceMapper/PizzaDTOMapper.java
@@ -0,0 +1,20 @@
+package ar.edu.undec.pizzeriaboundaries.Service.ServiceMapper;
+
+import excepciones.PizzaIncompletaException;
+import modelo.Pizza;
+
+public class PizzaDTOMapper {
+
+    public Pizza mapeoDTOCore(PizzaDTO pizzaDTO){
+        try {
+
+            return Pizza.factoryPizza(barrioDTO.getIdPizza(), barrioDTO.getNombre(), barrioDTO.getPrecio(), barrioDTO.getTiempoPrepareacion());
+        }catch (PizzaIncompletaException ex){
+            ex.printStackTrace();
+            return null;
+        }
+        
+    }
+
+
+}
