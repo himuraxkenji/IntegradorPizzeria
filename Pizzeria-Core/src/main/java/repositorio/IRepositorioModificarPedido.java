f676c2422b883330caed0cd98c2e62d022c6059b
juan asis <juan.asis1234gmail.com>
2019-11-20 01:14:16 -0300

    Modificacii¡on Test ModificarPedidoIT

--- Pizzeria-Core/src/main/java/repositorio/IRepositorioModificarPedido.java
+++ Pizzeria-Core/src/main/java/repositorio/IRepositorioModificarPedido.java
@@ -1,10 +1,12 @@
 package repositorio;
 
+import excepciones.PedidoIncompletoException;
+import excepciones.PizzaIncompletaException;
 import modelo.Pedido;
 
 public interface IRepositorioModificarPedido {
 
-    Pedido findByNroPedido(Integer numeroPedido);
+    Pedido findByNroPedido(Integer numeroPedido) throws PedidoIncompletoException, PizzaIncompletaException;
 
     Boolean actualizar(Pedido Pedido);
 
