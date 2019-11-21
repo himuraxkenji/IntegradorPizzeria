69c474b0b5727010bf680ce3cbfc8af5ed4641ab
rodrigo <rodrigo.oscar.alarcon@gmail.com>
2019-11-20 21:00:24 -0300

    Se Completa Test Unit Caso de uso Pizzas MAs Vendidas EntreFechas

--- Pizzeria-Core/src/main/java/repositorio/IRepositorioObtenerPizzasMasVendidasEntreFechas.java
+++ Pizzeria-Core/src/main/java/repositorio/IRepositorioObtenerPizzasMasVendidasEntreFechas.java
@@ -1,18 +1,14 @@
 package repositorio;
 
-
 import excepciones.FechaIncorrectaException;
 import excepciones.PedidoIncompletoException;
 import excepciones.PizzaIncompletaException;
+import modelo.Pizza;
 
 import java.time.LocalDateTime;
-
-import java.util.List;
-
+import java.util.HashMap;
 
 public interface IRepositorioObtenerPizzasMasVendidasEntreFechas {
 
-    List<String> obtenerPizzasMasVendidasEntreFechas(LocalDateTime fechaInicio, LocalDateTime fechaFin) throws FechaIncorrectaException, PedidoIncompletoException, PizzaIncompletaException;
-
-
+    HashMap<Pizza, Integer> obtenerPizzasMasVendidasEntreFechas(LocalDateTime fechaInicio, LocalDateTime fechaFin) throws FechaIncorrectaException, PedidoIncompletoException, PizzaIncompletaException;
 }
