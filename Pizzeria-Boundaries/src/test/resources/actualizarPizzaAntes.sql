59bfb27a88fe0f06bcafdf4ebe57d2e93fa72ae2
juan asis <juan.asis1234gmail.com>
2019-11-21 16:53:54 -0300

    correccion archivos sql
    el orden de los delete debe al ultimo las tablas referenciadas

--- Pizzeria-Boundaries/src/test/resources/actualizarPizzaAntes.sql
+++ Pizzeria-Boundaries/src/test/resources/actualizarPizzaAntes.sql
@@ -1,17 +1,11 @@
-delete from pedidos;
+delete from pizzas_por_pedido;
 delete from pizzas;
-delete from clientes;
-delete from barrios;
 
-alter sequence seq_pedidos restart with 1;
 alter sequence seq_pizzas restart with 1;
-alter sequence seq_clientes restart with 1;
-alter sequence seq_barrios restart with 1;
 
 insert into pizzas values(nextval('seq_pizzas'), 'Napolitana', 120.0, 15);
 insert into pizzas values(nextval('seq_pizzas'), 'Muzzarella', 200.0, 18);
 insert into pizzas values(nextval('seq_pizzas'), 'Fugazzeta', 230.0, 20);
 insert into pizzas values(nextval('seq_pizzas'), 'Especial', 250.0, 15);
-
 
 
