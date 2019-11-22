d67f56a7039961477888dfe62470b4bf12439bf3
Riperto Adriel Aaron <aaron.ariperto@gmai.com>
2019-11-22 11:12:25 -0300

    Debug: Pasa test de Data. SQL Cambiado

--- Pizzeria-Boundaries/src/test/resources/obtenerMontoIngresadoPorPedidoEntreFechasAntes.sql
+++ Pizzeria-Boundaries/src/test/resources/obtenerMontoIngresadoPorPedidoEntreFechasAntes.sql
@@ -1,13 +1,14 @@
-TRUNCATE TABLE clientes CASCADE;
-TRUNCATE TABLE pizzas CASCADE;
-TRUNCATE TABLE barrios CASCADE;
-TRUNCATE TABLE pedidos CASCADE;
-TRUNCATE TABLE pizzas_por_pedido CASCADE;
 
-ALTER SEQUENCE seq_barrios RESTART WITH 1;
-ALTER SEQUENCE seq_clientes RESTART WITH 1;
-ALTER SEQUENCE seq_pedidos RESTART WITH 1;
-ALTER SEQUENCE seq_pizzas RESTART WITH 1;
+delete from pizzas_por_pedido;
+delete from pedidos;
+delete from clientes;
+delete from barrios;
+delete from pizzas;
+
+alter SEQUENCE seq_pedidos restart with 1;
+alter SEQUENCE seq_clientes restart with 1;
+alter SEQUENCE seq_barrios restart with 1;
+alter SEQUENCE seq_pizzas restart with 1;
 
 INSERT INTO pizzas VALUES(nextval('seq_pizzas'), 'Comun', 150.0, 30);
 INSERT INTO pizzas VALUES(nextval('seq_pizzas'), 'Especial', 200.0, 30);
@@ -18,10 +19,10 @@ INSERT INTO barrios VALUES(NEXTVAL('seq_barrios'),'Paiman Sur');
 INSERT INTO clientes (id_cliente, direccion, documento, nombre, id_barrio) VALUES (NEXTVAL('seq_clientes'), 'San Juan 570','37492933', 'Manuel',1);
 INSERT INTO clientes (id_cliente, direccion, documento, nombre, id_barrio) VALUES (NEXTVAL('seq_clientes'), 'Santa Rosa 544','38222999', 'Federico',2);
 
-INSERT INTO pedidos(id_pedido,id_cliente,fecha,numero_pedido) VALUES(nextval('seq_pedidos'),1,'15/11/2019 21:30',1);
-INSERT INTO pedidos(id_pedido,id_cliente,fecha,numero_pedido) VALUES(nextval('seq_pedidos'),2,'18/11/2019 20:10',1);
-INSERT INTO pedidos(id_pedido,id_cliente,fecha,numero_pedido) VALUES(nextval('seq_pedidos'),2,'18/11/2019 20:30',2);
-INSERT INTO pedidos(id_pedido,id_cliente,fecha,numero_pedido) VALUES(nextval('seq_pedidos'),1,'19/11/2019 21:30',1);
+INSERT INTO pedidos VALUES(nextval('seq_pedidos'),to_date('15/11/2019 21:30','DD/MM/YYYY HH24:MI'),1,1);
+INSERT INTO pedidos VALUES(nextval('seq_pedidos'),to_date('18/11/2019 20:10','DD/MM/YYYY HH24:MI'),1,2);
+INSERT INTO pedidos VALUES(nextval('seq_pedidos'),to_date('18/11/2019 20:30','DD/MM/YYYY HH24:MI'),2,2);
+INSERT INTO pedidos VALUES(nextval('seq_pedidos'),to_date('19/11/2019 21:30','DD/MM/YYYY HH24:MI'),1,1);
 
 INSERT INTO pizzas_por_pedido VALUES(1,2);
 INSERT INTO pizzas_por_pedido VALUES(2,1);
